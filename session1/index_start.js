53a4195eceb39d2927d304457d12f339ce9ef4e9
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-03-15 19:31:07 +0200

    solved challenge 1,2,3

--- session1/index_start.js
+++ session1/index_start.js
@@ -3,8 +3,14 @@
 // ex. reverseString('hello') === 'olleh'
 
 function reverseString(str) {
-  return 'Let\'s Start'
+  const revStr = str
+    .split('')
+    .reverse()
+    .join('')
+  return revStr
 }
+
+
 
 
 
@@ -12,7 +18,13 @@ function reverseString(str) {
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
-function isPalindrome(str) {}
+function isPalindrome(str) {
+  const revStr = str
+    .split('')
+    .reverse()
+    .join('')
+  return str === revStr ? true : false
+}
 
 
 
@@ -20,31 +32,34 @@ function isPalindrome(str) {}
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
 
-function reverseInt(int) {}
+function reverseInt(int) {
+  const revInt = int.toString().split('').reverse().join('')
+  return parseInt(revInt) * Math.sign(int)
+}
 
 
 
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) {}
+function capitalizeLetters(str) { }
 
 
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
+function maxCharacter(str) { }
 
 
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() { }
 
 
 
 // Call Function
-const output = reverseString('hello');
+const output = reverseInt(-34);
 
 console.log(output);
